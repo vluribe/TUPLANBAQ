e9515b2a5d38b51a8866bd5ef6ea96aec12ca2a4
vluribe <vluribe@uninorte.edu.co>
2020-04-06 21:29:26 -0500

    Create conexiongen.php

---
+++ conexiongen.php
@@ -0,0 +1,7 @@
+           <?php
+            $servername = "localhost";
+            $database = "colegioe_tuplanbaq";
+            $username = "colegioe_laura";
+            // Create connection
+            $conn = mysqli_connect($servername, $username, '$Abcd1234$', $database);
+            ?>
